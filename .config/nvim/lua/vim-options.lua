877886f6307088734630ed403fe178be0451185a
Ahmed HABBACHI <ahmed.habbachi@outlook.com>
2024-05-16 18:31:17 +0100

    update vim mapping

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -9,5 +9,16 @@ vim.cmd("set relativenumber")
 
 
 vim.g.mapleader = ","
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set("n", "<Leader>bn", ":bn<CR>")
+vim.keymap.set("n", "<Leader>bb", ":bp<CR>")
+vim.keymap.set("n", "<Leader>bd", ":bd<CR>")
 
